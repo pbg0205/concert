5df204ee06a2172faeb01c3d56936923bf152f38
cooper <pbg0205@naver.com>
2025-01-02 14:31:45 +0900

    feat: 유저 잔액 조회/충전 MockAPI 작성

---
+++ src/main/java/com/cooper/concert/users/presentation/dto/response/UserBalanceCheckResponse.java
@@ -0,0 +1,13 @@
+package com.cooper.concert.users.presentation.dto.response;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
+public class UserBalanceCheckResponse {
+	private final Long balance;
+}
