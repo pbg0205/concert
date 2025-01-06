13abfad366a7f30386a35052762a3cb116e99740
cooper <pbg0205@naver.com>
2025-01-07 00:50:12 +0900

    feat: UserBalanceService 포인트 조회

--- src/test/java/com/cooper/concert/business/service/UserBalanceServiceTest.java
+++ src/test/java/com/cooper/concert/business/service/UserBalanceServiceTest.java
@@ -15,6 +15,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.cooper.concert.business.dto.response.UserBalanceChargeResult;
+import com.cooper.concert.business.dto.response.UserBalanceReadResult;
 import com.cooper.concert.business.errors.UserErrorCode;
 import com.cooper.concert.business.errors.UserErrorType;
 import com.cooper.concert.business.errors.exception.UserBalanceNotFoundException;
@@ -84,5 +85,54 @@ class UserBalanceServiceTest {
 
 		// then
 		assertThat(sut.balance()).isEqualTo(2000L);
+	}
+
+	@Test
+	@DisplayName("유저를 조회하지 못할 경우, UserNotFoundException 반환하며 포인트 조회 실패")
+	void 유저_조회_실패하면_포인트_조회_실패() {
+		// given
+		final UUID userAltId = UUID.randomUUID();
+
+		when(userRepository.findByAltId(any())).thenReturn(null);
+
+		// when, then
+		assertThatThrownBy(() -> userBalanceService.readUserBalance(userAltId))
+			.isInstanceOf(UserNotFoundException.class)
+			.extracting("errorType")
+			.satisfies(errorType -> assertThat(((UserErrorType)errorType).getErrorCode())
+				.isEqualTo(UserErrorCode.ERROR_USER04));
+	}
+
+	@Test
+	@DisplayName("유저 잔고 조회하지 못할 경우, UserBalanceNotFoundException 반환하며 포인트 조회 실패")
+	void 유저_잔고_조회_실패하면_포인트_조회_실패() {
+		// given
+		final UUID userAltId = UUID.randomUUID();
+
+		when(userRepository.findByAltId(any())).thenReturn(User.create("사용자1", userAltId));
+		when(userBalanceRepository.findByUserId(any())).thenReturn(null);
+
+		// when, then
+		assertThatThrownBy(() -> userBalanceService.chargePoint(userAltId, 1000L))
+			.isInstanceOf(UserBalanceNotFoundException.class)
+			.extracting("errorType")
+			.satisfies(errorType -> assertThat(((UserErrorType)errorType).getErrorCode())
+				.isEqualTo(UserErrorCode.ERROR_USER05));
+	}
+
+	@Test
+	@DisplayName("유저 포인트 조회 성공")
+	void 유저_포인트_조회_성공() {
+		// given
+		final UUID userAltId = UUID.randomUUID();
+
+		when(userRepository.findByAltId(any())).thenReturn(User.create("사용자1", userAltId));
+		when(userBalanceRepository.findByUserId(any())).thenReturn(UserBalance.create(1L, 1000L));
+
+		// when
+		final UserBalanceReadResult sut = userBalanceService.readUserBalance(userAltId);
+
+		// then
+		assertThat(sut.balance()).isEqualTo(1000L);
 	}
 }
