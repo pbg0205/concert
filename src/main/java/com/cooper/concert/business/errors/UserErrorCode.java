585b83624c02668a788a2463b945de160e5083a9
cooper <pbg0205@naver.com>
2025-01-06 20:56:15 +0900

    feat: UserBalanceService 포인트 충전

--- src/main/java/com/cooper/concert/business/errors/UserErrorCode.java
+++ src/main/java/com/cooper/concert/business/errors/UserErrorCode.java
@@ -1,9 +1,10 @@
 package com.cooper.concert.business.errors;
 
 public enum UserErrorCode {
-	ERROR_USER01, ERROR_USER02
 	ERROR_USER01,
 	ERROR_USER02,
 	ERROR_USER03,
+	ERROR_USER04,
+	ERROR_USER05,
 	ERROR_USER06
 }
