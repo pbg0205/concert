939ca732bc98d29a315973709c9c9bf96cd8093b
cooper <pbg0205@naver.com>
2025-01-06 14:28:22 +0900

    feat: querydsl JpaQueryFactory 추가

---
+++ src/main/java/com/cooper/concert/common/jpa/config/QueryDslConfig.java
@@ -0,0 +1,23 @@
+package com.cooper.concert.common.jpa.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class QueryDslConfig {
+
+	@PersistenceContext
+	private final EntityManager entityManager;
+
+	@Bean
+	public JPAQueryFactory jpaQueryFactory() {
+		return new JPAQueryFactory(entityManager);
+	}
+}
