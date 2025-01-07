aca6c6dc55089a436b26390acb08f8ebf5dc65e9
cooper <pbg0205@naver.com>
2025-01-08 03:32:34 +0900

    feat: WaitingQueueQueryRepository 토큰 활성화을 위한 조회

--- src/main/java/com/cooper/concert/domain/queues/infrastructure/rdb/WaitingQueueCommandRepositoryDsl.java
+++ src/main/java/com/cooper/concert/domain/queues/infrastructure/rdb/WaitingQueueCommandRepositoryDsl.java
@@ -2,6 +2,8 @@ package com.cooper.concert.domain.queues.infrastructure.rdb;
 
 import static com.cooper.concert.domain.queues.models.QQueueToken.queueToken;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -23,4 +25,12 @@ public class WaitingQueueCommandRepositoryDsl implements WaitingQueueCommandRepo
 			.where(queueToken.userId.eq(userId).and(queueToken.status.stringValue().eq(status)))
 			.fetchFirst();
 	}
+
+	@Override
+	public List<QueueToken> findAllByIds(final List<Long> accessibleQueueTokenIds) {
+		return jpaQueryFactory.selectFrom(queueToken)
+			.where(queueToken.id.in(accessibleQueueTokenIds))
+			.fetch();
+	}
+
 }
--- src/test/java/com/cooper/concert/domain/queues/repository/WaitingQueueCommandRepositoryTest.java
+++ src/test/java/com/cooper/concert/domain/queues/repository/WaitingQueueCommandRepositoryTest.java
@@ -1,6 +1,9 @@
 package com.cooper.concert.domain.queues.repository;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
+
+import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -38,5 +41,19 @@ class WaitingQueueCommandRepositoryTest {
 			softAssertions.assertThat(sut.getUserId()).isEqualTo(userId);
 			softAssertions.assertThat(sut.getStatus().name()).isEqualTo(tokenStatus);
 		});
+	}
+
+	@Test
+	@DisplayName("유저 식별자 목록를 입력하면 유저 토큰 목록 조회 성공")
+	@Sql("classpath:/sql/accessible_token_list.sql")
+	void 유저_식별자_목록를_입력하면_유저_토큰_목록_조회_성공() {
+		// given
+		final List<Long> ids = List.of(1007L, 1008L, 1009L);
+
+		// when
+		final List<QueueToken> sut = waitingQueueCommandRepository.findAllByIds(ids);
+
+		// then
+		assertThat(sut).hasSize(3);
 	}
 }
