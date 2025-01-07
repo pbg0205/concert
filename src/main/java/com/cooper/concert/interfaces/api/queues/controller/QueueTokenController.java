23ba12cfdb70f818576525e737d0153b0921f780
cooper <pbg0205@naver.com>
2025-01-08 00:54:09 +0900

    feat: QueueTokenController 토큰 발급

--- src/main/java/com/cooper/concert/interfaces/api/queues/controller/QueueTokenController.java
+++ src/main/java/com/cooper/concert/interfaces/api/queues/controller/QueueTokenController.java
@@ -1,24 +1,37 @@
 package com.cooper.concert.interfaces.api.queues.controller;
 
-import java.util.UUID;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
+
 import com.cooper.concert.common.api.support.response.ApiResponse;
+import com.cooper.concert.domain.queues.service.dto.QueueTokenIssueResult;
 import com.cooper.concert.interfaces.api.queues.dto.request.QueueTokenIssueRequest;
 import com.cooper.concert.interfaces.api.queues.dto.response.QueueTokenIssueResponse;
+import com.cooper.concert.interfaces.api.queues.usecase.QueueTokenIssueUseCase;
 
 @RestController
 @RequestMapping("/api/queue")
+@RequiredArgsConstructor
 public class QueueTokenController {
+
+	private final QueueTokenIssueUseCase queueTokenIssueUseCase;
 
 	@PostMapping("/token/issue")
 	public ResponseEntity<ApiResponse<QueueTokenIssueResponse>> generateToken(
 		@RequestBody final QueueTokenIssueRequest queueTokenIssueRequest) {
-		return ResponseEntity.ok(ApiResponse.success(new QueueTokenIssueResponse(UUID.randomUUID(), 10)));
+
+		final QueueTokenIssueResult queueTokenIssueResult =
+			queueTokenIssueUseCase.issueQueueToken(queueTokenIssueRequest.getUserId());
+
+		final QueueTokenIssueResponse queueTokenIssueResponse = new QueueTokenIssueResponse(
+			queueTokenIssueResult.tokenId(),
+			queueTokenIssueResult.waitingPosition());
+
+		return ResponseEntity.ok(ApiResponse.success(queueTokenIssueResponse));
 	}
 }
