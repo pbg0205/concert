db81d53041ddbefb084df1415393feb56476cdfc
cooper <pbg0205@naver.com>
2025-01-08 15:22:10 +0900

    feat: TokenScheduler 토큰 만료 스케줄러

--- src/main/java/com/cooper/concert/interfaces/schedules/queues/usecase/TokenSchedulerUseCase.java
+++ src/main/java/com/cooper/concert/interfaces/schedules/queues/usecase/TokenSchedulerUseCase.java
@@ -2,19 +2,27 @@ package com.cooper.concert.interfaces.schedules.queues.usecase;
 
 import java.time.LocalDateTime;
 
+import org.springframework.transaction.annotation.Transactional;
+
 import lombok.RequiredArgsConstructor;
 
 import com.cooper.concert.common.annotations.Facade;
+import com.cooper.concert.domain.queues.service.QueueTokenExpiredService;
 import com.cooper.concert.domain.queues.service.WaitingQueueService;
 
 @Facade
 @RequiredArgsConstructor
+@Transactional
 public class TokenSchedulerUseCase {
 
 	private final WaitingQueueService waitingQueueService;
+	private final QueueTokenExpiredService queueTokenExpiredService;
 
 	public Integer updateToProcessing(final LocalDateTime expiredAt) {
 		return waitingQueueService.updateToProcessing(expiredAt);
 	}
 
+	public Integer expireToken(final LocalDateTime expiredAt) {
+		return queueTokenExpiredService.updateToExpired(expiredAt);
+	}
 }
