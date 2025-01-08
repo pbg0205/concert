de0335ec5a96f52238b4018e3ab4297602f5800a
cooper <pbg0205@naver.com>
2025-01-09 01:10:51 +0900

    feat: QueueTokenService 활성화 토큰 유효성 검증
    - 활성화 토큰 검증 로직
    - 활성화 토큰 검증 실패 테스트 코드
    - 활성화 토큰 검증 성공 테스트 코드
     - TokenNotFoundException 상위 예외 멤버 변수 수정

--- src/main/java/com/cooper/concert/domain/queues/service/errors/exception/TokenException.java
+++ src/main/java/com/cooper/concert/domain/queues/service/errors/exception/TokenException.java
@@ -6,10 +6,10 @@ import com.cooper.concert.domain.queues.service.errors.TokenErrorType;
 
 @Getter
 public abstract class TokenException extends RuntimeException {
-	private final TokenErrorType tokenErrorType;
+	private final TokenErrorType errorType;
 
 	public TokenException(final TokenErrorType errorType) {
 		super(errorType.getMessage());
-		this.tokenErrorType = errorType;
+		this.errorType = errorType;
 	}
 }
