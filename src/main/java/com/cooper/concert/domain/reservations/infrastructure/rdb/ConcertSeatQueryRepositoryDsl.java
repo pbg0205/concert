30ce08c1ea2994402017b1a2bc650d73b0460f53
cooper <pbg0205@naver.com>
2025-01-09 04:11:05 +0900

    feat: ConcertSeatQueryRepository 예약 가능 좌석 조회

---
+++ src/main/java/com/cooper/concert/domain/reservations/infrastructure/rdb/ConcertSeatQueryRepositoryDsl.java
@@ -0,0 +1,31 @@
+package com.cooper.concert.domain.reservations.infrastructure.rdb;
+
+import static com.cooper.concert.domain.reservations.models.QConcertSeat.concertSeat;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import lombok.RequiredArgsConstructor;
+
+import com.cooper.concert.domain.reservations.service.repository.ConcertSeatQueryRepository;
+
+@Repository
+@RequiredArgsConstructor
+public class ConcertSeatQueryRepositoryDsl implements ConcertSeatQueryRepository {
+
+	private final JPAQueryFactory queryFactory;
+
+	@Override
+	public List<Long> findConcertSeatsByScheduleIdAndStatusAndPaging(
+		final Long scheduleId, final String status, final Integer offset, final Integer limit) {
+		return queryFactory.select(concertSeat.seatNumber)
+			.from(concertSeat)
+			.where(concertSeat.scheduleId.eq(scheduleId).and(concertSeat.status.stringValue().eq(status)))
+			.limit(limit)
+			.offset(offset)
+			.fetch();
+	}
+}
