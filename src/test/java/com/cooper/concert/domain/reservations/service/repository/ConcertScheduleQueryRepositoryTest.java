b61c8ee287f4eeab9accbd818d9590115aebc6a1
cooper <pbg0205@naver.com>
2025-01-09 04:11:34 +0900

    feat ConcertScheduleQueryRepository 스케줄 단일 조회

--- src/test/java/com/cooper/concert/domain/reservations/service/repository/ConcertScheduleQueryRepositoryTest.java
+++ src/test/java/com/cooper/concert/domain/reservations/service/repository/ConcertScheduleQueryRepositoryTest.java
@@ -1,11 +1,14 @@
 package com.cooper.concert.domain.reservations.service.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -20,7 +23,8 @@ import com.cooper.concert.base.listener.DataCleanUpExecutionListener;
 import com.cooper.concert.domain.reservations.service.dto.response.ConcertScheduleResult;
 
 @DataJpaTest
-@ComponentScan(basePackages = {"com.cooper.concert.domain.reservations.infrastructure.rdb",  "com.cooper.concert.common.jpa"})
+@ComponentScan(basePackages = {"com.cooper.concert.domain.reservations.infrastructure.rdb",
+	"com.cooper.concert.common.jpa"})
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @TestExecutionListeners(value = {
 	DataCleanUpExecutionListener.class}, mergeMode = TestExecutionListeners.MergeMode.MERGE_WITH_DEFAULTS)
@@ -48,5 +52,23 @@ class ConcertScheduleQueryRepositoryTest {
 			Arguments.of(1L, 10, 10, 10),
 			Arguments.of(1L, 20, 10, 5)
 		);
+	}
+
+	@Test
+	@DisplayName("콘서트 식별자를 통한 단일 콘서트 스케줄 조회 성공")
+	@Sql("classpath:sql/concert_schedule_available_dates_repository_sample_data.sql")
+	void 콘서트_식별자를_통한_단일_콘서트_스케줄_조회_성공() {
+		// given
+		final Long scheduleId = 6L; // 스케줄 날짜 : 2025-01-13T00:00
+
+		// when
+		final ConcertScheduleResult concertScheduleResult =
+			concertScheduleQueryRepository.findConcertScheduleResultById(scheduleId);
+
+		// then
+		assertSoftly(softAssertions -> {
+			softAssertions.assertThat(concertScheduleResult.concertScheduleId()).isEqualTo(scheduleId);
+			softAssertions.assertThat(concertScheduleResult.startDateTime()).isEqualTo(LocalDateTime.of(2025, 1, 13, 0, 0));
+		});
 	}
 }
