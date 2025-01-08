9cc3d896c449880d77974556e1ebdb1ae7500524
cooper <pbg0205@naver.com>
2025-01-08 15:58:22 +0900

    refactor: 스케줄링 타임 단위 변경

--- src/main/java/com/cooper/concert/interfaces/schedules/queues/scheduler/TokenScheduler.java
+++ src/main/java/com/cooper/concert/interfaces/schedules/queues/scheduler/TokenScheduler.java
@@ -1,6 +1,7 @@
 package com.cooper.concert.interfaces.schedules.queues.scheduler;
 
 import java.time.LocalDateTime;
+import java.util.concurrent.TimeUnit;
 
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,12 +18,12 @@ public class TokenScheduler {
 
 	private final TokenSchedulerUseCase tokenSchedulerUseCase;
 
-	@Scheduled(fixedRate = 60_000)
+	@Scheduled(fixedRate = 1, timeUnit = TimeUnit.MINUTES)
 	public Integer updateTokenToProcessing() {
 		return tokenSchedulerUseCase.updateToProcessing(LocalDateTime.now());
 	}
 
-	@Scheduled(fixedRate = 60_000)
+	@Scheduled(fixedRate = 1, timeUnit = TimeUnit.MINUTES)
 	public Integer updateTokenToExpired() {
 		return tokenSchedulerUseCase.expireToken(LocalDateTime.now());
 	}
