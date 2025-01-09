02af37b481a9a503bb7012d3097f0b4cd1173dcf
cooper <pbg0205@naver.com>
2025-01-10 03:05:44 +0900

    feat: PaymentProcessUseCase 결제 처리

---
+++ src/main/java/com/cooper/concert/interfaces/api/payments/usecase/PaymentProcessUseCase.java
@@ -0,0 +1,43 @@
+package com.cooper.concert.interfaces.api.payments.usecase;
+
+import java.util.UUID;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+
+import com.cooper.concert.common.annotations.Facade;
+import com.cooper.concert.domain.payments.service.PaymentProcessingService;
+import com.cooper.concert.domain.payments.service.dto.response.PaymentCompleteInfo;
+import com.cooper.concert.domain.payments.service.dto.response.PaymentProcessResult;
+import com.cooper.concert.domain.reservations.service.ConcertReservationService;
+import com.cooper.concert.domain.reservations.service.ConcertSeatReadService;
+import com.cooper.concert.domain.reservations.service.dto.response.ConcertReservationCompletedInfo;
+import com.cooper.concert.domain.reservations.service.dto.response.ConcertSeatPriceInfo;
+import com.cooper.concert.domain.users.service.UserBalanceUseService;
+
+@Facade
+@RequiredArgsConstructor
+@Transactional
+public class PaymentProcessUseCase {
+
+	private final ConcertReservationService concertReservationService;
+	private final ConcertSeatReadService concertSeatReadService;
+	private final UserBalanceUseService userBalanceUseService;
+	private final PaymentProcessingService paymentProcessingService;
+
+	public PaymentProcessResult processPayment(final UUID paymentAltId) {
+		final PaymentCompleteInfo paymentCompleteInfo = paymentProcessingService.completePayment(paymentAltId);
+
+		final ConcertReservationCompletedInfo reservationCompletedInfo =
+			concertReservationService.completeReservation(paymentCompleteInfo.reservationId());
+
+		final ConcertSeatPriceInfo concertSeatPriceInfo =
+			concertSeatReadService.findConcertSeatReadInfoById(reservationCompletedInfo.seatId());
+
+		userBalanceUseService.usePoint(reservationCompletedInfo.userId(), concertSeatPriceInfo.price());
+
+
+		return new PaymentProcessResult(reservationCompletedInfo.altId());
+	}
+}
