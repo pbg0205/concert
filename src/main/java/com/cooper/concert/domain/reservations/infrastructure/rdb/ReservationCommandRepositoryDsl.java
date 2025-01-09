64f6ff2e0fbbd3c9469b4b2c574a5b09aedc863a
cooper <pbg0205@naver.com>
2025-01-10 02:55:09 +0900

    feat: ReservationCommandRepository 대체키 기반 예약 조회

--- src/main/java/com/cooper/concert/domain/reservations/infrastructure/rdb/ReservationCommandRepositoryDsl.java
+++ src/main/java/com/cooper/concert/domain/reservations/infrastructure/rdb/ReservationCommandRepositoryDsl.java
@@ -1,6 +1,10 @@
 package com.cooper.concert.domain.reservations.infrastructure.rdb;
 
+import static com.cooper.concert.domain.reservations.models.QReservation.reservation;
+
 import org.springframework.stereotype.Repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import lombok.RequiredArgsConstructor;
 
@@ -12,9 +16,17 @@ import com.cooper.concert.domain.reservations.service.repository.ReservationComm
 public class ReservationCommandRepositoryDsl implements ReservationCommandRepository {
 
 	private final JpaReservationRepository reservationRepository;
+	private final JPAQueryFactory queryFactory;
 
 	@Override
 	public Reservation save(final Reservation reservation) {
 		return reservationRepository.save(reservation);
+	}
+
+	@Override
+	public Reservation findById(final Long id) {
+		return queryFactory.selectFrom(reservation)
+			.where(reservation.id.eq(id))
+			.fetchOne();
 	}
 }
--- src/test/java/com/cooper/concert/domain/reservations/service/repository/ReservationCommandRepositoryTest.java
+++ src/test/java/com/cooper/concert/domain/reservations/service/repository/ReservationCommandRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.cooper.concert.domain.reservations.service.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
 import java.util.UUID;
 
@@ -11,6 +12,7 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.jdbc.Sql;
 
 import com.cooper.concert.base.listener.DataCleanUpExecutionListener;
 import com.cooper.concert.domain.reservations.models.Reservation;
@@ -28,7 +30,7 @@ class ReservationCommandRepositoryTest {
 
 	@Test
 	@DisplayName("예약 저장 성공")
-	void Î예약_저장_성공() {
+	void 예약_저장_성공() {
 		// given
 		final UUID altId = UUID.fromString("019449f7-8f1c-78a8-8afa-40c273e4708a");
 		final Reservation reservation = Reservation.createPendingReservation(1L, 1L, altId);
@@ -38,5 +40,22 @@ class ReservationCommandRepositoryTest {
 
 		// then
 		assertThat(sut.getId()).isEqualTo(reservation.getId());
+	}
+
+	@Test
+	@DisplayName("대체키 기반 예약 조회 성공")
+	@Sql("classpath:sql/reservation_repository.sql")
+	void 대체키_기반_예약_조회_성공() {
+		// given
+		final Long reservationId = 1L;
+
+		// when
+		final Reservation sut = reservationCommandRepository.findById(reservationId);
+
+		// then
+		assertSoftly(softAssertions -> {
+			softAssertions.assertThat(sut.getId()).isNotNull();
+			softAssertions.assertThat(sut.getId()).isEqualTo(reservationId);
+		});
 	}
 }
