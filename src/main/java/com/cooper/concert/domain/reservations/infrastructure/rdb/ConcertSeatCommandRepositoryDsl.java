c6f5ea3389881d11f638b3fef811f6900245adae
cooper <pbg0205@naver.com>
2025-01-10 05:29:59 +0900

    feat: ConcertSeatCommandRepository 좌석 점유 취소 목록 조회

--- src/main/java/com/cooper/concert/domain/reservations/infrastructure/rdb/ConcertSeatCommandRepositoryDsl.java
+++ src/main/java/com/cooper/concert/domain/reservations/infrastructure/rdb/ConcertSeatCommandRepositoryDsl.java
@@ -2,6 +2,8 @@ package com.cooper.concert.domain.reservations.infrastructure.rdb;
 
 import static com.cooper.concert.domain.reservations.models.QConcertSeat.concertSeat;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -22,5 +24,12 @@ public class ConcertSeatCommandRepositoryDsl implements ConcertSeatCommandReposi
 		return queryFactory.selectFrom(concertSeat)
 			.where(concertSeat.id.eq(id))
 			.fetchOne();
+	}
+
+	@Override
+	public List<ConcertSeat> findAllByIdsAndSeatStatus(final List<Long> concertSeatIds, final String seatStatus) {
+		return queryFactory.selectFrom(concertSeat)
+			.where(concertSeat.id.in(concertSeatIds).and(concertSeat.status.stringValue().eq(seatStatus)))
+			.fetch();
 	}
 }
--- src/test/java/com/cooper/concert/domain/reservations/service/repository/ConcertSeatCommandRepositoryTest.java
+++ src/test/java/com/cooper/concert/domain/reservations/service/repository/ConcertSeatCommandRepositoryTest.java
@@ -2,6 +2,8 @@ package com.cooper.concert.domain.reservations.service.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,5 +39,21 @@ class ConcertSeatCommandRepositoryTest {
 
 		// then
 		assertThat(sut.getId()).isEqualTo(id);
+	}
+
+	@Test
+	@DisplayName("점유 취소를 위한 콘서트 좌석 조회")
+	@Sql("classpath:sql/concert_seats_cancel_repository.sql")
+	void 점유_취소를_위한_콘서트_좌석_조회() {
+		// given
+		List<Long> seatIds = List.of(1L, 2L, 3L, 4L, 5L, 6L);
+		String seatStatus = "UNAVAILABLE";
+
+		// when
+		final List<ConcertSeat> sut =
+			concertSeatCommandRepository.findAllByIdsAndSeatStatus(seatIds, seatStatus);
+
+		// then
+		assertThat(sut).hasSize(4);
 	}
 }
