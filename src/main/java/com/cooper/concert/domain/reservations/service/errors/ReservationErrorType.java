9f5b0b047e50ccce85b83abec248ae7382eee1d9
cooper <pbg0205@naver.com>
2025-01-10 02:54:24 +0900

    feat: Reservation 상태 변화 로직
    - 예약 취소 로직
    - 예약 완료 로직

--- src/main/java/com/cooper/concert/domain/reservations/service/errors/ReservationErrorType.java
+++ src/main/java/com/cooper/concert/domain/reservations/service/errors/ReservationErrorType.java
@@ -6,7 +6,9 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum ReservationErrorType {
-	CONCERT_SEAT_OCCUPIED(ReservationErrorCode.ERROR_RESERVATION01, "해당 좌석은 점유되어 있습니다");
+	CONCERT_SEAT_OCCUPIED(ReservationErrorCode.ERROR_RESERVATION01, "해당 좌석은 점유되어 있습니다"),
+	RESERVATION_CANCELED(ReservationErrorCode.ERROR_RESERVATION02, "해당 예약은 취소 되었습니다."),
+	RESERVATION_RESERVED(ReservationErrorCode.ERROR_RESERVATION03, "해당 예약은 이미 예약 되었습니다.");
 
 	private final ReservationErrorCode errorCode;
 	private final String message;
--- src/test/java/com/cooper/concert/domain/reservations/models/ReservationTest.java
+++ src/test/java/com/cooper/concert/domain/reservations/models/ReservationTest.java
@@ -1,6 +1,7 @@
 package com.cooper.concert.domain.reservations.models;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
 import java.util.UUID;
 
@@ -21,7 +22,46 @@ class ReservationTest {
 		Reservation sut = Reservation.createPendingReservation(userId, seatId, altId);
 
 		// then
-		assertThat(sut.getStatus()).isEqualTo(ReservationStatus.PENDING);
+		assertThat(sut).extracting("status").isEqualTo(ReservationStatus.PENDING);
 	}
 
+	@Test
+	@DisplayName("예약 완료 성공")
+	void 예약_완료_성공() {
+		// given
+		final Long userId = 1L;
+		final Long seatId = 1L;
+		final UUID altId = UUID.fromString("019449c5-0b39-7680-9599-2ca3536facae"); // uuid ver7
+
+		Reservation sut = Reservation.createPendingReservation(userId, seatId, altId);
+
+		// when
+		final boolean completed = sut.complete();
+
+		// then
+		assertSoftly(softAssertions -> {
+			softAssertions.assertThat(completed).isTrue();
+			softAssertions.assertThat(sut).extracting("status").isEqualTo(ReservationStatus.RESERVED);
+		});
+	}
+
+	@Test
+	@DisplayName("예약 취소 성공")
+	void 예약_취소_성공() {
+		// given
+		final Long userId = 1L;
+		final Long seatId = 1L;
+		final UUID altId = UUID.fromString("019449c5-0b39-7680-9599-2ca3536facae"); // uuid ver7
+
+		Reservation sut = Reservation.createPendingReservation(userId, seatId, altId);
+
+		// when
+		final boolean canceled = sut.cancel();
+
+		// then
+		assertSoftly(softAssertions -> {
+			softAssertions.assertThat(canceled).isTrue();
+			softAssertions.assertThat(sut).extracting("status").isEqualTo(ReservationStatus.CANCELED);
+		});
+	}
 }
