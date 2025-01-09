64bff6e987f0c0f1f6839034715341cc0d8e892a
cooper <pbg0205@naver.com>
2025-01-10 05:29:26 +0900

    feat: PaymentCancelService 결제 제거

--- src/main/java/com/cooper/concert/domain/payments/service/repository/PaymentCommandRepository.java
+++ src/main/java/com/cooper/concert/domain/payments/service/repository/PaymentCommandRepository.java
@@ -1,5 +1,6 @@
 package com.cooper.concert.domain.payments.service.repository;
 
+import java.util.List;
 import java.util.UUID;
 
 import com.cooper.concert.domain.payments.models.Payment;
@@ -7,4 +8,5 @@ import com.cooper.concert.domain.payments.models.Payment;
 public interface PaymentCommandRepository {
 	Payment save(Payment payment);
 	Payment findByAltId(UUID paymentAltId);
+	List<Payment> findAllByReservationIds(List<Long> reservationIds, String paymentStatus);
 }
