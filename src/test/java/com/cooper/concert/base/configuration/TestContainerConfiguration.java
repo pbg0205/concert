a7f3d8f4707b0d27d03adb2b84a9938291e0d445
cooper <pbg0205@naver.com>
2025-01-12 23:20:10 +0900

    refactor: DB 테스트 환경 TestContainer 로 변경

---
+++ src/test/java/com/cooper/concert/base/configuration/TestContainerConfiguration.java
@@ -0,0 +1,38 @@
+package com.cooper.concert.base.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+import jakarta.annotation.PreDestroy;
+
+@Testcontainers
+@Configuration
+public class TestContainerConfiguration {
+
+	@Container
+	static MySQLContainer<?> MYSQL_CONTAINER = new MySQLContainer<>(DockerImageName.parse("mysql:8.0"));
+
+	static {
+		MYSQL_CONTAINER.start();
+	}
+
+	@DynamicPropertySource
+	static void configureProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", MYSQL_CONTAINER::getJdbcUrl);
+		registry.add("spring.datasource.username", MYSQL_CONTAINER::getUsername);
+		registry.add("spring.datasource.password", MYSQL_CONTAINER::getPassword);
+	}
+
+	@PreDestroy
+	void preDestroy() {
+		if (MYSQL_CONTAINER.isRunning()) {
+			MYSQL_CONTAINER.stop();
+		}
+	}
+
+}
