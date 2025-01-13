ca13d91754115ecf6e4067e9acf944025bbe13a4
cooper <pbg0205@naver.com>
2025-01-13 14:59:24 +0900

    feat: HTTP Request 로깅 로직 추가(#37)
    - MDC 를 사용한 request-id 추가
    - request url, header, body 로깅 추가

---
+++ src/main/java/com/cooper/concert/common/api/components/filter/request/RequestBodyCachedServletInputStream.java
@@ -0,0 +1,41 @@
+package com.cooper.concert.common.api.components.filter.request;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import jakarta.servlet.ReadListener;
+import jakarta.servlet.ServletInputStream;
+
+public class RequestBodyCachedServletInputStream extends ServletInputStream {
+
+	private final InputStream cachedBodyInputStream;
+
+	public RequestBodyCachedServletInputStream(byte[] cachedBody) {
+		this.cachedBodyInputStream = new ByteArrayInputStream(cachedBody);
+	}
+
+	@Override
+	public boolean isFinished() {
+		try {
+			return cachedBodyInputStream.available() == 0;
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	public boolean isReady() {
+		return true;
+	}
+
+	@Override
+	public void setReadListener(ReadListener readListener) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public int read() throws IOException {
+		return cachedBodyInputStream.read();
+	}
+}
