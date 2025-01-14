77e9357dd8bdca263f67d88eec327342b94118ac
cooper <pbg0205@naver.com>
2025-01-14 15:22:36 +0900

    fix: UUID 포함 request body 예외 처리 핸들러 추가(#39)
    - 증상
       - 잘못된 request body 로 인한 status 500 에러 반환
    - 원인
       - request body 를 읽는 과정에서 잘못된 UUID 포맷으로 예외 반환
         (HttpMessageNotReadableException)
    - 개선
       - HttpMessageNotReadableException 예외 처리 핸들러 추가
       - 예외 코드 정의(ERROR_COMMON02)

--- src/main/java/com/cooper/concert/common/api/support/error/CommonErrorType.java
+++ src/main/java/com/cooper/concert/common/api/support/error/CommonErrorType.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum CommonErrorType {
 	ERROR_UNKNOWN(CommonErrorCode.ERROR_COMMON01, "알 수 없는 예외가 발생했습니다."),
-	;
+	ERROR_MESSAGE_UNREADABLE(CommonErrorCode.ERROR_COMMON02, "읽을 수 없는 HTTP 메시지 입니다.");
 
 	private final CommonErrorCode errorCode;
 	private final String message;
