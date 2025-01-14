b5d89e05ee28f1f6e9f6410b65afb88806c1913a
cooper <pbg0205@naver.com>
2025-01-14 15:22:36 +0900

    fix: UUID 포함 request body 예외 처리 핸들러 추가(#39)
    - 증상
       - 잘못된 request body 로 인한 status 500 에러 반환
    - 원인
       - request body 를 읽는 과정에서 잘못된 UUID 포맷으로 예외 반환
         (HttpMessageNotReadableException)
    - 개선
       - HttpMessageNotReadableException 예외 처리 핸들러 추가
       - 예외 코드 정의(ERROR_COMMON02)

--- src/main/java/com/cooper/concert/common/api/components/handler/GlobalExceptionHandler.java
+++ src/main/java/com/cooper/concert/common/api/components/handler/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.cooper.concert.common.api.components.handler;
 
 import org.springframework.core.annotation.Order;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -21,6 +22,15 @@ public class GlobalExceptionHandler {
 
 		final CommonErrorType errorType = CommonErrorType.ERROR_UNKNOWN;
 		return ResponseEntity.internalServerError()
+			.body(ApiResponse.error(errorType.getErrorCode().name(), errorType.getMessage()));
+	}
+
+	@ExceptionHandler(value = HttpMessageNotReadableException.class)
+	public ResponseEntity<ApiResponse<?>> handleHttpMessageNotReadableException(HttpMessageNotReadableException exception) {
+		log.error("exception : {}, message : {}", exception.getClass().getSimpleName(), exception.getMessage());
+
+		final CommonErrorType errorType = CommonErrorType.ERROR_MESSAGE_UNREADABLE;
+		return ResponseEntity.badRequest()
 			.body(ApiResponse.error(errorType.getErrorCode().name(), errorType.getMessage()));
 	}
 }
--- src/main/java/com/cooper/concert/common/api/support/error/CommonErrorCode.java
+++ src/main/java/com/cooper/concert/common/api/support/error/CommonErrorCode.java
@@ -1,5 +1,7 @@
 package com.cooper.concert.common.api.support.error;
 
 public enum CommonErrorCode {
-	ERROR_COMMON01
+	ERROR_COMMON01,
+	ERROR_COMMON02
+	;
 }
