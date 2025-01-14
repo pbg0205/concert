1843f78dbbe0b45920378bbc3b5803f984af3802
cooper <pbg0205@naver.com>
2025-01-14 16:47:11 +0900

    fix: GlobalExceptionHandler stackTrace error log 추가(#39)
    - 증상
       - GlobalExceptionHandler 로 전달된 예외의 원인 파악이 어려웠음
    - 원인
       - 원인 예외에 관한 정보를 파악하기 어려웠음.
    - 개선
       - stackTrace 정보를 GlobalExceptionHandler 에 로그에 추가

--- src/main/java/com/cooper/concert/common/api/components/handler/GlobalExceptionHandler.java
+++ src/main/java/com/cooper/concert/common/api/components/handler/GlobalExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.cooper.concert.common.api.components.handler;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.core.annotation.Order;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -18,7 +21,7 @@ public class GlobalExceptionHandler {
 
 	@ExceptionHandler(value = RuntimeException.class)
 	public ResponseEntity<ApiResponse<?>> handleRuntimeException(RuntimeException exception) {
-		log.error("exception : {}, message : {}", exception.getClass().getSimpleName(), exception.getMessage());
+		logError(exception);
 
 		final CommonErrorType errorType = CommonErrorType.ERROR_UNKNOWN;
 		return ResponseEntity.internalServerError()
@@ -26,11 +29,23 @@ public class GlobalExceptionHandler {
 	}
 
 	@ExceptionHandler(value = HttpMessageNotReadableException.class)
-	public ResponseEntity<ApiResponse<?>> handleHttpMessageNotReadableException(HttpMessageNotReadableException exception) {
-		log.error("exception : {}, message : {}", exception.getClass().getSimpleName(), exception.getMessage());
+	public ResponseEntity<ApiResponse<?>> handleHttpMessageNotReadableException(
+		HttpMessageNotReadableException exception) {
+		logError(exception);
 
 		final CommonErrorType errorType = CommonErrorType.ERROR_MESSAGE_UNREADABLE;
 		return ResponseEntity.badRequest()
 			.body(ApiResponse.error(errorType.getErrorCode().name(), errorType.getMessage()));
+	}
+
+	private void logError(final RuntimeException exception) {
+		final List<String> stackTraces = Arrays.stream(exception.getStackTrace())
+			.map(StackTraceElement::toString)
+			.toList();
+
+		log.error("exception : {}, message : {}\n stackTraces: {}",
+			exception.getClass().getSimpleName(),
+			exception.getMessage(),
+			String.join("\n", stackTraces));
 	}
 }
