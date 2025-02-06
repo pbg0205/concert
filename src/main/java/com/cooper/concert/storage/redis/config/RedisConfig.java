cbb8b3360df627e34c8b2ecd770871e6a6e86778
cooper <pbg0205@naver.com>
2025-02-06 21:50:20 +0900

    refactor: 서버 종료 시 redis keys 제거(#74)

--- src/main/java/com/cooper/concert/storage/redis/config/RedisConfig.java
+++ src/main/java/com/cooper/concert/storage/redis/config/RedisConfig.java
@@ -1,5 +1,7 @@
 package com.cooper.concert.storage.redis.config;
 
+import java.util.Set;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -7,6 +9,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+import jakarta.annotation.PreDestroy;
 import lombok.RequiredArgsConstructor;
 
 @Configuration
@@ -24,4 +27,9 @@ public class RedisConfig {
 		return redisTemplate;
 	}
 
+	@PreDestroy
+	public void destroy() {
+		final Set<String> keys = redisTemplate().keys("*");
+		redisTemplate().delete(keys);
+	}
 }
