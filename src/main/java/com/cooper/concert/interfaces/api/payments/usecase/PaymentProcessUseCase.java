e391fa112df2806350f5d98c402979a9d743546a
cooper <pbg0205@naver.com>
2025-02-13 20:23:54 +0900

    feat: 결제 처리 부가 로직 이벤트 기반 방식 변경
    - 토큰 만료 로직 이벤트 기반 방식 변경

--- src/main/java/com/cooper/concert/interfaces/api/payments/usecase/PaymentProcessUseCase.java
+++ src/main/java/com/cooper/concert/interfaces/api/payments/usecase/PaymentProcessUseCase.java
@@ -2,20 +2,21 @@ package com.cooper.concert.interfaces.api.payments.usecase;
 
 import java.util.UUID;
 
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 
-import com.cooper.concert.interfaces.components.annotations.Facade;
 import com.cooper.concert.domain.payments.service.PaymentProcessingService;
 import com.cooper.concert.domain.payments.service.dto.response.PaymentCompleteInfo;
 import com.cooper.concert.domain.payments.service.dto.response.PaymentProcessResult;
-import com.cooper.concert.domain.queues.service.QueueTokenExpiredService;
+import com.cooper.concert.domain.queues.service.dto.event.QueueTokenExpireEvent;
 import com.cooper.concert.domain.reservations.service.ConcertReservationService;
 import com.cooper.concert.domain.reservations.service.ConcertSeatReadService;
 import com.cooper.concert.domain.reservations.service.dto.response.ConcertReservationCompletedInfo;
 import com.cooper.concert.domain.reservations.service.dto.response.ConcertSeatPriceInfo;
 import com.cooper.concert.domain.users.service.UserBalanceUseService;
+import com.cooper.concert.interfaces.components.annotations.Facade;
 
 @Facade
 @RequiredArgsConstructor
@@ -26,7 +27,7 @@ public class PaymentProcessUseCase {
 	private final ConcertSeatReadService concertSeatReadService;
 	private final UserBalanceUseService userBalanceUseService;
 	private final PaymentProcessingService paymentProcessingService;
-	private final QueueTokenExpiredService queueTokenExpiredService;
+	private final ApplicationEventPublisher applicationEventPublisher;
 
 	public PaymentProcessResult processPayment(final UUID paymentAltId, final Long userId) {
 		final PaymentCompleteInfo paymentCompleteInfo = paymentProcessingService.completePayment(paymentAltId);
@@ -39,7 +40,7 @@ public class PaymentProcessUseCase {
 
 		userBalanceUseService.usePoint(reservationCompletedInfo.userId(), concertSeatPriceInfo.price());
 
-		queueTokenExpiredService.expireCompleteToken(userId);
+		applicationEventPublisher.publishEvent(new QueueTokenExpireEvent(userId));
 
 		return new PaymentProcessResult(reservationCompletedInfo.altId());
 	}
