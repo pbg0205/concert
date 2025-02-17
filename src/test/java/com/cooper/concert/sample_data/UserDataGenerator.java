df09686ddaf6d4a470c0a9554e93d38b9e32e3e6
cooper <pbg0205@naver.com>
2025-02-17 20:32:44 +0900

    refactor: 유저 아이디 샘플 추가

--- src/test/java/com/cooper/concert/sample_data/UserDataGenerator.java
+++ src/test/java/com/cooper/concert/sample_data/UserDataGenerator.java
@@ -3,7 +3,6 @@ package com.cooper.concert.sample_data;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.UUID;
 
 import com.fasterxml.uuid.Generators;
 
@@ -14,7 +13,8 @@ public class UserDataGenerator {
 
 		try (
 			BufferedWriter userWriter = new BufferedWriter(new FileWriter("sample-data/user_small.csv"));
-			BufferedWriter balanceWriter = new BufferedWriter(new FileWriter("sample-data/user_balance_small.csv"))
+			BufferedWriter balanceWriter = new BufferedWriter(new FileWriter("sample-data/user_balance_small.csv"));
+			BufferedWriter altIdWriter = new BufferedWriter(new FileWriter("k6/user_alt_id.csv"))
 		) {
 			// user 테이블 CSV 헤더
 			userWriter.write("id,name,alt_id,created_at,modified_at\n");
@@ -26,7 +26,8 @@ public class UserDataGenerator {
 			// 사용자 및 user_balance 데이터 생성
 			for (int userId = 1; userId <= totalUsers; userId++) {
 				String name = "user" + String.format("%05d", userId);  // 예: user00001
-				String altId = convertUuidToHex(Generators.timeBasedEpochGenerator().generate().toString());  // UUID -> 16진수 문자열 변환
+				final String altIdStrVal = Generators.timeBasedEpochGenerator().generate().toString();
+				String altId = convertUuidToHex(altIdStrVal);  // UUID -> 16진수 문자열 변환
 				long createdAt = startTimestamp + userId;  // 각 사용자마다 타임스탬프 1초씩 증가
 
 				// user 데이터 작성
@@ -34,6 +35,9 @@ public class UserDataGenerator {
 
 				// user_balance 데이터 작성 (포인트는 고정 1000)
 				balanceWriter.write(String.format("%d,%d,1000,%d,%d\n", balanceId++, userId, createdAt, createdAt));
+
+				// alt_id 데이터 작성
+				altIdWriter.write(String.format("%s\n", altIdStrVal));  // alt_id만 기록
 			}
 
 			System.out.println("Generated user_small.csv and user_balance_small.csv with 100,000 users.");
@@ -43,11 +47,7 @@ public class UserDataGenerator {
 		}
 	}
 
-	// UUID를 16진수 문자열로 변환하는 메서드
 	public static String convertUuidToHex(String uuid) {
-		UUID parsedUuid = UUID.fromString(uuid);
-		long mostSignificantBits = parsedUuid.getMostSignificantBits();
-		long leastSignificantBits = parsedUuid.getLeastSignificantBits();
-		return String.format("%016x%016x", mostSignificantBits, leastSignificantBits);
+		return uuid.replace("-", "");
 	}
 }
