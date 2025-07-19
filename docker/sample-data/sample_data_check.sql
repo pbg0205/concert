c990072591f8f33837729f405dffc2437c9b4a72
cooper <pbg0205@naver.com>
2025-07-19 23:47:12 +0900

    build: 부하테스트 데이터 삭제 및 갯수 조회 로직 추가

--- docker/sample-data/sample_data_check.sql
+++ docker/sample-data/sample_data_check.sql
@@ -2,11 +2,15 @@
 SHOW VARIABLES LIKE 'secure_file_priv';
 
 ## select concerts_count
+select count(id) from user as user_count;
+select count(id) from user_balance as user_balance_count;
 select count(id) from concert as concert_count;
 select count(id) from concert_schedule as concert_schedule_count;
 select count(id) from concert_seat as concert_seat_count;
 
 ## truncate concerts
+truncate user;
+truncate user_balance;
 truncate concert;
 truncate concert_schedule;
 truncate concert_seat;
